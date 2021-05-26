77b61b4805d754718acc254af70b9ba292e97042
Whitney <59493718+whitneyharper@users.noreply.github.com>
2021-05-26 17:13:17 -0400

    Add login route

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -4,5 +4,6 @@ const router = express.Router();
 const AuthController = require('../controllers/authController');
 
 router.post('/signup', AuthController.registerNewUser);
+router.post('/login', AuthController.loginUser);
 
 module.exports = router;
