7150181d9a9ca4133580f057b69e9eb915ddad08
Whitney <59493718+whitneyharper@users.noreply.github.com>
2021-05-28 00:23:56 -0400

    Update eventRoutes,js

--- src/routes/eventRoutes.js
+++ src/routes/eventRoutes.js
@@ -1,15 +1,16 @@
 const express = require('express');
 const router = express.Router();
-const EventController = require('../controllers/eventControllers')
+const EventController = require('../controllers/eventControllers');
+const {authenticateUser, checkIfAdmin} = require('../middlewares/authentication')
 //POST REQUEST TO CREATE A NEW EVENT
-router.post('/events', EventController.createNewEvent);
+router.post('/events', authenticateUser, EventController.createNewEvent);
 // GET REQUEST TO FETCH ALL EVENTS
-router.get('/events', EventController.fetchEvents);
+router.get('/events', authenticateUser, EventController.fetchEvents);
 // GET REQUEST TO FETCH A SINGLE EVENT
-router.get('/events/:_id', EventController.fetchSingleEvent);
+router.get('/events/:_id', authenticateUser, EventController.fetchSingleEvent);
 // PUT REQUEST TO UPDATE A EVENT
-router.put('/events/:_id', EventController.updateSingleEvent);
+router.put('/events/:_id', authenticateUser, checkIfAdmin, EventController.updateSingleEvent);
 //DELETE A REQUEST TO DELETE A EVENT
-router.delete('/events/:id', EventController.deleteSingleEvent);
+router.delete('/events/:id', authenticateUser, checkIfAdmin, EventController.deleteSingleEvent);
 
 module.exports = router;
